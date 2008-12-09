fbd89d2f1e15ca3b3679fab995e930ce56298f1c
Kyle Maxwell <kyle@58.flood.pivotallabs.com>
2008-12-09 15:14:15 -0800

    project cleanup

---
+++ bootstrap.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+autoreconf
+bison -yd
--- old.build.sh
+++
@@ -1,3 +0,0 @@
-yacc -d -t parser.y
-lex -o scanner.yy.c scanner.l
-gcc -std=c99 -lfl -L/opt/local/lib -I/opt/local/include -ljson -largp obstack.c printbuf.c kstring.c y.tab.c scanner.yy.c dexter.c dexterc_main.c -o dexterc
