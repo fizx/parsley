42ad0bbb0fe93300c00052aca114d2db38da0b49
Kyle Maxwell <kyle@172.16.1.35>
2008-12-29 19:10:44 -0800

    missing util files

---
+++ util.c
@@ -0,0 +1,23 @@
+#include "util.h"
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+
+FILE* dex_fopen(char* name, char* mode) {
+	FILE* fo;
+	if(!strcmp("-", name)) {
+		if(!strcmp("w", name)) {
+			fo = stdout;
+		} else {
+			fo = stdin;
+		}
+	} else {
+		fo = fopen(name, mode);
+	}
+	if(fo == NULL) {		
+    fprintf(stderr, "Cannot open file %s, error %d, %s\n", name, errno, strerror(errno));
+		exit(1);
+	}
+	return fo;	
+}
---
+++ util.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+FILE* dex_fopen(char*, char*);
