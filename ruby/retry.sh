0ed58c388fffc9fd30d664dd802ad64a320c24b8
Kyle Maxwell <kyle@kyle-maxwells-macbook.local>
2009-01-03 13:40:36 -0800

    direct dependencies

--- ruby/retry.sh
+++
@@ -1,7 +0,0 @@
-cd ext/ &&
-gcc  -I/opt/local/include -I/usr/local/include  -L/opt/local/lib -L/usr/local/lib   -I. -I. -I/System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/1.8/universal-darwin9.0 -I. -I/usr/include/libxml2 -L/usr/lib -lxslt -lxml2 -lz -lpthread -licucore -lm  -fno-common -arch i386 -Os -pipe -fno-common  -c dexterous.c &&
-make &&
-cd .. &&
-gem build dexterous.gemspec  &&
-sudo gem install dexterous &&
-irb -rdexterous
