2d95ff049e2208e7f08bb6f46b623ed966ae590a
Kyle Maxwell <kyle@kyle-maxwells-macbook.local>
2009-01-04 14:02:59 -0800

    test stubs for python

---
+++ python/test/test_dexpy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import unittest
+
+class TestDexPy(unittest.TestCase):
+	
+	def testnothing(self):
+		self.assertEqual(1, 1)
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	unittest.main()
