2287f08979e97d95035411b8ceda9522f3ce9f21
Kyle Maxwell <kyle@kyle-maxwells-macbook.local>
2009-02-24 14:53:05 -0800

    fix pointer type in dex_mem

--- dex_mem.c
+++ dex_mem.c
@@ -1,14 +1,20 @@
 #include "dex_mem.h"
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
 
-struct obstack * dex_obstack = NULL;
+static struct obstack dex_obstack;
+static bool dex_obstack_initialized = false;
 
 void dex_collect() {
 	obstack_free(&dex_obstack, NULL);
-  dex_obstack = NULL;
+	obstack_init(&dex_obstack);
 }
 
 void * dex_alloc(int size) {
-  if(dex_obstack == NULL) obstack_init(&dex_obstack);
+  if(!dex_obstack_initialized) {
+		obstack_init(&dex_obstack);
+		dex_obstack_initialized = true;
+	}
 	return obstack_alloc(&dex_obstack, size);
 }
